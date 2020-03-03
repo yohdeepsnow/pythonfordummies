5abcd4524efa8245209a7c2d26e6e32bfed6d68b
yohan <mulionoyohan@gmail.com>
2020-03-03 07:19:50 +0000

    initialized file

---
+++ main.py
@@ -0,0 +1,23 @@
+def penjumlahan(a,b):
+	#isi fungsi penjumlahan diletakkan di variable total
+	return total
+
+def pengurangan(a,b):
+	#isi fungsi pengurangan diletakkan di variable total
+	return total
+
+def perkalian(a,b):
+	#isi fungsi perkalian diletakkan di variable total 
+	return total
+
+def pembagian(a,b):
+	#isi fungsi pembagian diletakkan di variable total
+	return total
+
+def main():
+	print penjumlahan(5,5)
+	print pengurangan(10,5)
+	print perkalian(2,2)
+	print pembagian(100,5) 
+
+main()
